c01d349c848b11553ff810d5e1c0bc4d83e079f5
imeza <mbisrael@gmail.com>
2023-02-21 11:05:40 -0600

    [ADD] new module structure

---
+++ demo-widget/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*- 
+{
+    "name" : 'Prueba de snippet odoo',
+    "version" : '15.0',
+    "author" : 'exdoo.mx/imeza',
+    "category" : 'website',
+    "website" : "exdoo.mx",
+    "license" : "AGPL-3",
+    "description" : """
+        Este módulo agrega con copia a la plantilla de correo sin crearlo como contacto.
+        
+        Si tiene dudas, quiere reportar algún error o mejora póngase en contacto con nosotros: info@exdoo.mx
+        """,
+    "depends" : ['base','website'],
+    "data" : [
+        # "views/mail_compose_message_view.xml",
+        ],
+    "installable" : True,
+    "auto_install" : False,
+    'price': 0.0,
+    'currency': 'MXN',
+}
